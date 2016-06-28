e28eff3c47f173eb05da84d094e5ee900450d57d
Jim Cote <theJimCote@gmail.com>
2016-06-28 19:32:43 -0400

    External data file. Fade in, no fade out.

---
+++ archentides-data.js
@@ -0,0 +1,52 @@
+var archentidesData = {
+    sessions: [{
+        title: 'Game 1',
+        subtitle: 'Sunken Hopes',
+        steps: [{
+            bg: 'black',
+            image: '00',
+            copyTitle: 'A Disasterous Voyage',
+            copy: 'A merchant ship, “The Broadsword” sets sail north from Westgate. On it, among others, are the soon-to-be companions—the would-be heroes in our story.'
+        },{
+            bg: 'black',
+            image: '101',
+            copyTitle: 'Landfall',
+            copy: 'The party reaches land after a grueling two-mile swim. They collapse on a rocky shore.'
+        },{
+            bg: '#969696',
+            image: '102',
+            copyTitle: 'Survivors',
+            copy: 'Morning arrives too soon. The 11 companions size each other up—and their situation—while scavenging for food and supplies in the floating debris of the shipwreck. Five distant hills are made out. The tallest is the one furthest north. The top quarter of it is just a giant slab of exposed, gray rock.'
+        },{
+            bg: '#969696',
+            image: '103',
+            copyTitle: 'Two Fires',
+            copy: 'With no immediate threats, the party makes a small campfire to partially dry their soggy clothes. A separate trail of smoke is spotted rising from somewhere about two miles north along the coast. After a few hours of rest it’s decided the party will investigate the source of the smoke.'
+        },{
+            bg: '#969696',
+            image: '104',
+            copyTitle: 'Impending Rain',
+            copy: 'A thick fog has rolled in by the time the party reaches the source of the smoke.'
+        },{
+            bg: '#969696',
+            image: '105',
+            copyTitle: 'Disturbed Campsite',
+            copy: 'A small stream emerging from a natural crevice empties into the ocean near a campsite where a tent is partially collapsed. There is a rowboat nearby. A campfire is smoldering and there are signs of a scuffle around the tent. Some supplies are found, along with a <link>golden tablet<overlay> locked inside an iron chest.'
+        },{
+            bg: '#969696',
+            image: '106',
+            copyTitle: 'Tentaclams',
+            copy: 'Two dessicated corpses are found in a shallow mound of sand. One wears an iron medallion with the symbol of a spiked gauntlet holding six barbed arrows. Soon thereafter, five giant clams with long, tentacle-like tongues emerge from the sand and attack. The creatures are dealt with while some of the party make a hasty retreat to the entrance of the crevice. It has started to rain. The party decides to follow the crevice east while looking for game and shelter.'
+        },{
+            bg: '#969696',
+            image: '107',
+            copyTitle: 'A Night’s Rest',
+            copy: 'The air is cooler in the crevice but its natural walls offer a little protection from the wind and rain. Eventually the party reaches a narrowing where a land bridge has turned this part of the crevice into a natural tunnel. The narrow tunnel is steep but the entrance affords a suitable spot to camp for the night.'
+        },{
+            bg: '#969696',
+            image: '108',
+            copyTitle: 'Giant Spiders',
+            copy: 'In the morning the party continues on, climbing the steep tunnel. 200 feet in, a bright spot ahead indicates the crevice opening to the sky again. But those in the lead suddenly step into a patch of sticky web on the ground. Two monstrous spiders, both the size of large dogs, attack from the ceiling of the tunnel. The spiders are put down after a short fight, but not before Korben suffers a poisonous bite. He is weakened, but able to press on.'
+        }]
+    }]
+};
